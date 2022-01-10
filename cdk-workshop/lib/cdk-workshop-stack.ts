77b6f218d796ce6481aa91b7fb7aa940c850b070
RichardWatkins1 <richardwatkins1@gmail.com>
2022-01-10 13:09:36 +0000

    grant lambda access

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,8 +15,13 @@ export class CdkWorkshopStack extends Stack {
       architecture: lambda.Architecture.ARM_64,
     })
 
-    new apigw.LambdaRestApi(this, 'restAPI', {
-      handler: hello,
-    })
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
   }
 }
--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -1,4 +1,3 @@
-import { Stack } from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
@@ -9,8 +8,7 @@ export interface HitCounterProps {
   downstream: lambda.IFunction;
 }
 
-
-export class HitCounter extends Stack {
+export class HitCounter extends Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
   
@@ -26,10 +24,11 @@ export class HitCounter extends Stack {
       handler: 'hitcounter.handler',
       code: lambda.Code.fromAsset('lambda'),
       environment: {
-          DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-          HITS_TABLE_NAME: table.tableName
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
       }
     });
-    
+
+    table.grantReadWriteData(this.handler)
   }
 }
